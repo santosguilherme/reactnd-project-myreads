77e12591aadc2d584db0621f80bc90b7c6ca1596
Guilherme Santos <sntguilherme@gmail.com>
2017-10-23 11:16:20 -0200

    refactor: Book transformed into Stateless Functional Component

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -1,53 +1,48 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import BookshelfChanger from '../BookshelfChanger/BookshelfChanger';
 
 import './Book.css';
 
-class Book extends Component {
-    handleBookshelfChanger = shelf => {
-        const {book, onUpdateBook} = this.props;
 
+const Book = ({book, onUpdateBook}) => {
+    const handleBookshelfChanger = shelf => {
         onUpdateBook({...book, shelf});
     };
+    const {
+        title,
+        shelf,
+        authors = [],
+        imageLinks
+    } = book;
+    const bookCoverStyle = {width: 128, height: 193, backgroundColor: '#ccc'};
 
-    render() {
-        const {book} = this.props;
-        const {
-            title,
-            shelf,
-            authors = [],
-            imageLinks
-        } = book;
-        const bookCoverStyle = {width: 128, height: 193, backgroundColor: '#ccc'};
-
-        if (imageLinks) {
-            delete bookCoverStyle.backgroundColor;
-            bookCoverStyle.backgroundImage = `url("${imageLinks.thumbnail}")`;
-        }
-
-        return (
-            <div className="book">
-                <div className="book-top">
-                    <div className="book-cover" style={bookCoverStyle}/>
-                    <BookshelfChanger
-                        shelf={shelf}
-                        onChangeOption={this.handleBookshelfChanger}
-                    />
-                </div>
-                <div className="book-title">
-                    {title}
-                </div>
-                <div className="book-authors">
-                    {authors.map(author => (
-                        <span key={author}>{author}</span>
-                    ))}
-                </div>
-            </div>
-        );
+    if (imageLinks) {
+        delete bookCoverStyle.backgroundColor;
+        bookCoverStyle.backgroundImage = `url("${imageLinks.thumbnail}")`;
     }
-}
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={bookCoverStyle}/>
+                <BookshelfChanger
+                    shelf={shelf}
+                    onChangeOption={handleBookshelfChanger}
+                />
+            </div>
+            <div className="book-title">
+                {title}
+            </div>
+            <div className="book-authors">
+                {authors.map(author => (
+                    <span key={author}>{author}</span>
+                ))}
+            </div>
+        </div>
+    );
+};
 
 Book.propTypes = {
     book: PropTypes.object.isRequired,
